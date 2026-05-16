5f6368487242dbb9a34cec41390cb3378c0387f6
Timur8949889484 <137862969+Timur8949889484@users.noreply.github.com>
2026-05-16 17:04:09 +0600

    Update main.jsx

--- src/main/main.jsx
+++ src/main/main.jsx
@@ -1,16 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// 1. Импортируем нужные инструменты из TanStack Query
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import App from "./App.jsx";
 
-// 2. Создаем экземпляр клиента (это наше хранилище кэша)
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* 3. Оборачиваем App и передаем клиент в props */}
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
